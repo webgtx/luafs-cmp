a5c256aade97224d641e75d6243236a5b52f7542
David Jhonson <alx.zolotarov@gmail.com>
2022-09-09 14:24:07 +0200

    Update: args features was added

--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 package.path = package.path .. ";?.lua"
 require "header"
 
-local meta_lines = lines_from("m.dat")
-local alpha_lines = lines_from("a.dat") 
+if arg[1] then else return print("waiting for files :/") end
+
+local meta_lines = lines_from(arg[1])
+local alpha_lines = lines_from(arg[2]) 
 
 for i, m_v in pairs(meta_lines) do
 	for i, a_v in pairs(alpha_lines) do
-		if m_v == a_v then print("OMG THEY ARE SAME !!! >:)\n".. i, a_v .." == ".. m_v .."\n-----") end
+		if m_v == a_v then print(i, a_v .." == ".. m_v .."\n-----") end
 	end
 end
 
